3c0d303ef5d29e0921a96d72ee2fcc0bf79cce8a
j.nick.terry <j.nick.terry@d8b90d80-bb63-11dd-bed2-db724ec49875>
2008-12-24 17:26:51 +0000

    this is just a temporary checkin, it shouldn't break anybody

---
+++ trunk/src/ammo/physics/physic.cpp
@@ -0,0 +1,66 @@
+#include "physic.hpp"
+
+namespace ammo
+{
+
+namespace //anonymous
+{
+	class NullPhysicImpl : public PhysicImpl
+	{
+	public:
+		virtual b2Vec2 GetPosition(void) const
+		{ return b2Vec2_zero; }
+
+		virtual void SetPosition(const b2Vec2& pos) { }
+
+		virtual b2Vec2 GetVelocity(void) const
+		{ return b2Vec2_zero; }
+
+		virtual void SetVelocity(const b2Vec2& pos) { }
+
+		virtual float GetTheta(void) const
+		{ return 0.f; }
+
+		virtual void SetTheta(float theta) { }
+
+		virtual float GetOmega(void) const
+		{ return 0.f; }
+
+		virtual void SetOmega(float theta) { }
+	};
+
+} // anonymous namespace
+
+Physic::Physic( void )
+ : m_pimpl( new NullPhysicImpl )
+{ }
+
+Physic::Physic( const PhysicPimpl& pimpl )
+ : m_pimpl( pimpl )
+{ }
+
+b2Vec2 Physic::GetPosition( void ) const
+{ return m_pimpl->GetPosition();	}
+
+void Physic::SetPosition( const b2Vec2& pos )
+{ m_pimpl->SetPosition( pos ); }
+
+b2Vec2 Physic::GetVelocity( void ) const
+{ return m_pimpl->GetVelocity();	}
+
+void Physic::SetVelocity( const b2Vec2& pos )
+{ m_pimpl->SetVelocity(pos); }
+
+float Physic::GetTheta( void ) const
+{ return m_pimpl->GetTheta();	}
+
+void Physic::SetTheta( float theta )
+{ m_pimpl->SetTheta(theta); }
+
+float Physic::GetOmega( void ) const
+{ return m_pimpl->GetOmega();	}
+
+void Physic::SetOmega( float theta )
+{ m_pimpl->SetOmega(); }
+
+} // ammo
---
+++ trunk/src/ammo/physics/physic_sys.hpp
@@ -0,0 +1,32 @@
+#ifndef AMMO_PHYSICS_PHYSIC_SYS_HPP_INCLUDED
+#define AMMO_PHYSICS_PHYSIC_SYS_HPP_INCLUDED
+
+#include "ammo/physics/physic.hpp"
+#include "ammo/physics/physic_blue_print.fwd.hpp"
+
+namespace ammo
+{
+	class PhysicSys
+	{
+	public:
+		template< typename BP >
+		void AddBluePrint( const std::string& name, const BP& bp );
+
+		virtual bool IsPhysic( const std::string& name ) const =0;
+
+		virtual Physic GetPhysic( const std::string& name ) =0;
+
+		virtual void Update( float dt ) =0;
+
+	private:
+		virtual void add_bp_ptr( const std::string& name, const PhysicBluePrint_ptr& bp ) =0;
+	};
+
+	template< typename BP >
+	void AddBluePrint( const std::string& name, const BP& bp )
+	{
+		PhysicBluePrint_ptr new_bp = new BP(bp);
+		add_bp_ptr( new_bp );
+	}
+}
+#endif //  AMMO_PHYSICS_PHYSIC_SYS_HPP_INCLUDED
