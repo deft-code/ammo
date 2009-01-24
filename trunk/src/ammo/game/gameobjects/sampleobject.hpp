80930333d9b8f7de898040f3ac2950d6c8a38d41
PhillipSpiess <PhillipSpiess@d8b90d80-bb63-11dd-bed2-db724ec49875>
2009-01-24 21:53:27 +0000

    Added some comments to the sampleobject

--- trunk/src/ammo/game/gameobjects/sampleobject.hpp
+++ trunk/src/ammo/game/gameobjects/sampleobject.hpp
@@ -19,15 +19,26 @@ namespace ammo
 		 _timer(2)
 	 {	}
 
+    // Update contains all the logic for the object. 
     void Update(float deltaTime);    
+    // Puts data into the bitstream. This is called automatically by Raknet. Put in all the information you need
+    // to be sent on serialization
     bool Serialize(RakNet::BitStream* bitStream, RakNet::SerializationContext* serializationContext);
+    // Puts data into the bitstream. This is called when the object is first created, Raknet is expecting you to serialize an indicator
+    // of which object type you are serializing first, then any data for that object.
     bool SerializeConstruction(RakNet::BitStream* bitStream, RakNet::SerializationContext* serializationContext);
+    // Deserializes the data sent via the Serialize Method
     void Deserialize(RakNet::BitStream* bitStream, RakNet::SerializationType serializationType, SystemAddress sender, RakNetTime timestamp);
+    // After the object is completely registered on client/server, this method is called to load up any graphics, or do any post constructor
+    // initialization.
     void OnRegisterComplete();
   private:
+    // Logic variables
     int _myVal;
     float _timer;
+    // Graphics token
     ammo::Graphic _sprite;
+    // Sound token
     ammo::Sound _sound;
   };
 }
