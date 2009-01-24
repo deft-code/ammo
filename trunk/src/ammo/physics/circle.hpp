505badb772fc2da7b37c4a0171b6b6e0fb942ecc
j.nick.terry <j.nick.terry@d8b90d80-bb63-11dd-bed2-db724ec49875>
2009-01-24 22:33:06 +0000

    circle exists now

---
+++ trunk/src/ammo/physics/circle.hpp
@@ -0,0 +1,18 @@
+#ifndef AMMO_PHYSICS_CIRCLE_HPP_INCLUDED
+#define AMMO_PHYSICS_CIRCLE_HPP_INCLUDED
+
+#include "ammo/physics/physic_blue_print.hpp"
+#include <Box2D.h>
+
+namespace ammo
+{
+	class Circle : public PhysicBluePrint
+	{
+	public:
+		b2BodyDef body_blueprint;
+		b2CircleDef circle_blueprint;
+
+		virtual PhysicPimpl Instantiate(b2World& world) const;
+	};
+}
+#endif // AMMO_PHYSICS_CIRCLE_HPP_INCLUDED
