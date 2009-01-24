3daf6110f213d012ffaba5c6efa441b5a8d674bf
j.nick.terry <j.nick.terry@d8b90d80-bb63-11dd-bed2-db724ec49875>
2009-01-24 22:33:06 +0000

    circle exists now

---
+++ src/ammo/physics/circle.cpp
@@ -0,0 +1,17 @@
+#include "circle.hpp"
+
+#include "single_body_impl.hpp"
+
+namespace ammo
+{
+	PhysicPimpl Circle::Instantiate( b2World& world ) const
+	{
+		b2Body* body = world.CreateBody( &body_blueprint );
+		PhysicPimpl ptr( new SingleBodyImpl( body ) );
+
+		body->CreateShape( &circle_blueprint );
+		body->SetMassFromShapes();
+
+		return ptr;
+	}
+}
