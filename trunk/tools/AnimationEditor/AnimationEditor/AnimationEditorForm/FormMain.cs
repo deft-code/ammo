05fa26f6f7f62d68d95f5daf0334864347336ea0
jeremy_rehkop@hotmail.com <jeremy_rehkop@hotmail.com@d8b90d80-bb63-11dd-bed2-db724ec49875>
2009-04-30 04:02:43 +0000

    Started over, had a better idea for the layout of the form. I think its much better now.

---
+++ trunk/tools/AnimationEditor/AnimationEditor/AnimationEditorForm/FormMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using XNAControl;
+
+namespace AnimationEditor
+{
+    public enum ProgramMode
+    {
+        Sprite, Animation
+    }
+
+    public partial class AnimationEditorForm : Form
+    {
+        private ProgramMode mode = ProgramMode.Sprite;
+
+        private XNAGraphicsControl spriteGraphics;
+        private XNAGraphicsControl animationGraphics;
+
+        private Timer drawTimer;
+
+        public AnimationEditorForm()
+        {
+            this.InitializeComponent();
+            this.InitializeApp();
+        }
+
+        private void InitializeApp()
+        {
+            this.spriteGraphics = new XNAGraphicsControl(this.panel_SpriteGraphics);
+            this.animationGraphics = new XNAGraphicsControl(this.panel_AnimationGraphics);
+            PrimitiveDrawer.Initialize(this.spriteGraphics.Graphics.GraphicsDevice);
+
+            this.drawTimer = new Timer();
+            this.drawTimer.Tick += new EventHandler(drawTimer_Tick);
+            this.drawTimer.Interval = 1000 / 60;
+            this.drawTimer.Start();
+        }
+    }
+}
---
+++ trunk/tools/AnimationEditor/AnimationEditor/Types/SpriteSheet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AnimationEditor
+{
+    public class SpriteSheet
+    {
+        private String m_name;
+        private String m_path;
+        private Texture2D m_image;
+        private Single m_zoom;
+        private Vector2 m_location;
+
+        public SpriteSheet() {
+            this.m_zoom = 1.0f;
+            this.m_location = Vector2.Zero; }
+
+        public String Name {
+            get { return this.m_name; }
+            set { this.m_name = value; } }
+
+        public String Path {
+            get { return this.m_path; }
+            set { this.m_path = value; } }
+
+        public Texture2D Image {
+            get { return this.m_image; }
+            set { this.m_image = value; } }
+
+        public Single Zoom {
+            get { return this.m_zoom; } }
+
+        public Vector2 Location {
+            get { return this.m_location; }
+            set { this.m_location = value; } }
+
+        public Vector2 Center {
+            get { return new Vector2(this.m_image.Width / 2, this.m_image.Height / 2); } }
+
+        public void ZoomIn() {
+            if (this.m_zoom < 10.00f)
+                this.m_zoom += 0.25f; }
+
+        public void ZoomOut() {
+            if (this.m_zoom > 0.25f)
+                this.m_zoom -= 0.25f; }
+    }
+}
